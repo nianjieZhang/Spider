ea119d886ec92d690a8983a7e169200e06739a8d
nianjieZhang <38093989+nianjieZhang@users.noreply.github.com>
2020-01-23 15:29:28 +0800

    Update 01.py
    Add some comments

--- 01.py
+++ 01.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bs = BeautifulSoup(html.read(),"html.parser")
+html = urlopen("http://pythonscraping.com/pages/page1.html") # 导入urlopen函数向服务器发起请求获取HTML页面
+bs = BeautifulSoup(html.read(),"html.parser") # 使用BeautifulSoup解析HTML页面
 
 print(bs.h1)
